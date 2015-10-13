3874edeaebcbdc7071ebb69166de5318b6f12a76
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2015-10-13 18:09:43 +1100

    added inline html response and begun post request handling

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ var url = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
+    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request received for: " + pathname);
+
+    request.setEncoding("utf8");
+
+    request.addListener("data", function(postChunk) {
+      postData += postChunk;
+      console.log("Received POST data chunk '" + postChunk + "'.");
+    });
+   
+    request.addListener("end", function() {
+      route(handle, pathname, response, postData);
+    });
 
     route(handle, pathname, response);
   }
